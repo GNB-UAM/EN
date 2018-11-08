8584782f96ab3a2a7ce267a2b98d8c7ce73ed56e
TheBarto <agftotg10@gmail.com>
2018-11-08 22:59:01 +0100

    Eliminadas inconsistencias de espacios y tabulaciones

--- newGUI/Controller.py
+++ newGUI/Controller.py
@@ -3,7 +3,7 @@
 import new_GUI as NG
 
 class Controller(QtWidgets.QMainWindow):
-    
+	
 	def __init__(self, parent=None):
 		QtWidgets.QMainWindow.__init__(self,parent)
 		self.ui = Ui_MainWindow()
@@ -12,33 +12,39 @@ class Controller(QtWidgets.QMainWindow):
 		self.expand=False
 		self.show()
 
-		self.labels,self.lines = [self.ui.Tendencia,self.ui.TempSensor,self.ui.ModoEjecucion,self.ui.VariacionTemperatura,self.ui.TempeaturaMaxima,self.ui.TemperaturaMinima,self.ui.PeriodoSenialReferencia,self.ui.LimMaximoSup,self.ui.LimMinimoSup,self.ui.LimMaximoInf,self.ui.LimMinimoInf,self.ui.Alfa,self.ui.PicoMaximo,self.ui.PicoMinimo],[self.ui.TendenciaEdit,self.ui.TempSensorEdit,self.ui.ModoEjecucionEdit,self.ui.VariacionTemperaturaEdit,self.ui.TempeaturaMaximaEdit,self.ui.TemperaturaMinimaEdit,self.ui.PeriodoSenialReferenciaEdit,self.ui.LimMaximoSupEdit,self.ui.LimMinimoSupEdit,self.ui.LimMaximoInfEdit,self.ui.LimMinimoInfEdit,self.ui.AlfaEdit,self.ui.PicoMaximoEdit,self.ui.PicoMinimoEdit]
+		self.labels,self.lines = [self.ui.Tendencia,self.ui.TempSensor,self.ui.ModoEjecucion,
+			self.ui.VariacionTemperatura,self.ui.TempeaturaMaxima,self.ui.TemperaturaMinima,self.ui.PeriodoSenialReferencia,
+			self.ui.LimMaximoSup,self.ui.LimMinimoSup,self.ui.LimMaximoInf,self.ui.LimMinimoInf,self.ui.Alfa,self.ui.PicoMaximo,
+			self.ui.PicoMinimo],[self.ui.TendenciaEdit,self.ui.TempSensorEdit,self.ui.ModoEjecucionEdit,
+			self.ui.VariacionTemperaturaEdit,self.ui.TempeaturaMaximaEdit,self.ui.TemperaturaMinimaEdit,
+			self.ui.PeriodoSenialReferenciaEdit,self.ui.LimMaximoSupEdit,self.ui.LimMinimoSupEdit,self.ui.LimMaximoInfEdit,
+			self.ui.LimMinimoInfEdit,self.ui.AlfaEdit,self.ui.PicoMaximoEdit,self.ui.PicoMinimoEdit]
 
-		self.Modulation.on_Modulation_currentIndexChanged.connect(self.seleccionar_modulacion))
+		self.Modulation.on_Modulation_currentIndexChanged.connect(self.seleccionar_modulacion)
+		self.reiniciar_valores()
 
 	def seleccionar_modulacion(self):
 
 		selected = self.iu.Modulation.currentText();
 		if selected == "Puro":
-		    self.reiniciar_valores()
+			self.reiniciar_valores()
 		elif selected == "Regresion":
-		    for i in range(0,2):
-			self.labels[i].show()
-			self.lines[i].show()
+			ini,fin = 0,2
 		elif selected == "Martinelli":
-		    for i in range(2,6):
-			self.labels[i].show()
-			self.lines[i].show()
+			ini,fin = 2,6
 		elif selected == "PID":
-		    for i in range(6,len(self.labels)):
+			ini,fin = 6,len(self.labels)
+
+		for i in range(6,len(self.labels)):
 			self.labels[i].show()
 			self.lines[i].show()
+
 
 	def reiniciar_valores(self):
 
 		for label,line in zip(self.labels,self.lines):
-		    label.hide()
-		    line.hide()
+			label.hide()
+			line.hide()
 
 	
 	def iniciar_experimento(self):
