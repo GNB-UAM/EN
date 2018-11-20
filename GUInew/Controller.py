c8c9a875c136ea96d6dbf4aa9103fb4a3fb747d3
TheBarto <agftotg10@gmail.com>
2018-11-21 00:17:07 +0100

    Añado cambios finales a la UI

--- GUInew/Controller.py
+++ GUInew/Controller.py
@@ -7,6 +7,8 @@ import sys
 from PyQt5.QtWidgets import QFileDialog,QMessageBox
 from PyQt5.QtCore import *
 import os
+import paramiko
+
 class Controller(QtWidgets.QMainWindow):
         
         def __init__(self, parent=None):
@@ -39,12 +41,132 @@ class Controller(QtWidgets.QMainWindow):
                 self.conexion_tab.OKButton.clicked.connect(self.SSH_obtener_parametros_conexion)
                 self.conexion_tab.CheckButton.clicked.connect(self.SSH_comprobar_conexion)
                 self.conexion_tab.ResetButton.clicked.connect(self.SSH_resetear_parametros_conexion)
+                self.plot_tab.SFichero.clicked.connect(self.seleccionar_fichero_plot)
+                self.plot_tab.AnadirButton.clicked.connect(self.seleccionar_fichero_plot_SSH)
                 self.ssh_user,self.ssh_password,self.ssh_address,self.ssh_port,self.ssh_path = None,None,None,None,None
+                self.MAXSUCCION = 100
+                self.MINSUCCION = 30
+                self.MAXHEAT = 100
+                self.MINHEAT = 1
+                self.MINSWICHT = 1
+                self.MINTENDENCIA = 0
+                self.MINTIEMPO = 0
+                self.MINSAMPLESINIO = 10
+                self.files_plot = []
 
                 self.reiniciar_widgets()
-           
+
+        def comprobacion_datos_introducidos(mod,experiment_data_dict):
+    
+            #Caso base, opciones comunes
+            for suc,sw,sini in zip(experiment_data_dict[tc.SUCCION],experiment_data_dict[tc.SWESTIM],experiment_data_dict[tc.SINICIO]):
+                if suc > self.MAXSUCCION or suc < self.MINSUCCION or sw < self.MINSWICHT or (mod != 3 and sini < self.MINSAMPLESINIO):
+                    print ('PARAMETROS(S) INCORRECTO(S).\n Por favor revise los parametros introducidos y vuelva a empezar')
+                    exit(1)
+
+            #Caso de regresion
+            if mod == 2:
+                for tend,he in zip(experiment_data_dict[tc.TEND],experiment_data_dict[tc.HTSENSOR]):
+                    if he < self.MINHEAT or he > self.MAXHEAT or tend < self.MINTENDENCIA:
+                        print ('PARAMETROS(S) INCORRECTO(S).\n Por favor revise los parametros introducidos y vuelva a empezar')
+                        exit(1)
+
+        def start(self):
+            dict = {}
+            for label,entry in zip(self.ui.PuroLabelsLayout.children(),self.ui.PuroEntriesLayout.children()):
+                dict[label.objectName()] = entry.objectName()
+
+            for widget in self.modulations:
+                if widget.isVisible() == True:
+                    for label,entry in zip(widget.children()[1:int((len(widget.children())+1)/2)],widget.children()[int((len(widget.children())+1)/2):]):
+                        dict[label.objectName()] = entry.objectName()
+
+            try:
+                mod,dict = tratamiento_fichero_configuracion(dict)
+            except:
+                QMessageBox.critical(self,"Error","Error ocurrido al tratar los datos. Reviselos")
+                return
+
+            # Realizo un Pickle -> IMPORTANTE: CUIDADO CON LOS PICKLES, NO ABRIR NINGUNO DESCONOCIDO. RIESGO PARA LA SEGURIDAD DEL ORDENADOR
+            fileObject = open("pickle_dic.pkl","w")
+            pickle.dump([mod,dict],fileObject)
+            fileObject.close()
+
+            if self.ui.SSHcheckBox.isChecked() == False:
+                self.comprobacion_datos_introducidos(mod,dict)
+                modul = tipos_modulacion[mod](dict)
+                path = modul.iniciar_captura_datos()
+            else:
+                t = paramiko.Transport((self.ssh_address,int(self.ssh_port)))
+                t.connect(username=self.ssh_user, password=self.ssh_password)
+                sftp = paramiko.SFTPClient.from_transport(t)
+                sftp.put("pickle_dic.pkl",self.ssh_path+"pickle_dic.pkl")
+                t.close()
+                paramiko.util.log_to_file('paramilo.log')
+                ssh = paramiko.SSHClient()
+                ssh.load_system_host_keys()
+                ssh.connect(self.ssh_address,int(self.ssh_port),self.ssh_user,self.ssh_password)
+                command = 'cd '+self.ssh_path+' && sudo python3 PyHuele.py --remote'
+                stdin,stdout,stderr = ssh.exec_command(command)
+                ssh.close()
+                os.remove("pickle_dic.pkl")
+                   
+        def limpiar_opciones_plot(self,text):
+            if text == True: 
+                self.plot_tab.Texto.clear()
+                self.files_plot.clear()
+            for checkBox in self.plot_tab.checkBoxs.children():
+                checkBox.setChecked(False)
+            self.plot_tab.SSHFicheroEdit.clear()
+
+        def representar_datos(self):
+            self.limpiar_opciones_plot()
+            self.ax.clear()
+            for c,list in enumerate(self.files):
+                if len(files) == 3:
+                    file,options = self.obtener_fichero_plot_SSH(list)
+                else:
+                    file,options = list
+
+                data = np.genfromtxt(file,skipe_header=1,delimiter=' ').T
+                for c in options: ax.plot(data[c],color=c)
+                
+        def obtener_fichero_plot_SSH(self,filename):
+
+
         def habilitar_SSH(self):
-            self.SSHButton.setEnabled(True) if self.ui.SSHcheckBox.isChecked() == True else self.SSHButton.setEnabled(False)
+            self.ui.SSHButton.setEnabled(True) if self.ui.SSHcheckBox.isChecked() == True else self.SSHButton.setEnabled(False)
+            return
+
+        def habilidar_SSH_plot(self):
+            self.plot_tab.SSHPlotWidget.hide() if self.plot_tab.SSHcheckBox.isChecked() == True else self.plot_tab.SSHPlotWidget.show()
+            return
+
+        def seleccionar_fichero_plot(self):
+
+            filename = QFileDialog.getOpenFileName()[0]
+            self.seleccionar_opciones_plot(filename)
+
+        def seleccionar_fichero_plot_SSH(self):
+
+            filename = self.plot_tab.SSHFicheroEdit.text()
+            self.seleccionar_opciones_plot()
+
+        def seleccionar_opciones_plot(self,filename):
+
+            columns,name = [],[]
+            for pos,checkBox in enumerate(self.plot_tab.checkBoxsLayout.children()):
+                if checkBox.isChecked() == True:
+                    columns.append(pos+1)
+                    name.append(checkBox.text())
+
+            if len(columns) == 0:
+                QMessageBox.critical(self,"Error","Debe seleccionar una columna para su representacion")
+                return
+
+            self.plot_tab.Texto.append("%s\n\t%s\n"%(filename,name))
+            self.files_plot.append([filename,columns])
+            limpiar_opciones_plot(False)
             return
 
         def conexion(self):
@@ -71,7 +193,6 @@ class Controller(QtWidgets.QMainWindow):
                 QMessageBox.warning(self,"Conexion Erronea","El destino no se encuentra")
             
             return
-
 
         def seleccionar_modulacion(self):
 
@@ -121,7 +242,6 @@ class Controller(QtWidgets.QMainWindow):
                     else:
                         entries.append(self.ui.ExperimentLayout.itemAt(i).widget().text())
             
-
         def iniciar_experimento(self):
                 
                 diccionario = {}
@@ -134,8 +254,8 @@ class Controller(QtWidgets.QMainWindow):
             filename = QFileDialog.getOpenFileName()
             file = open(filename[0],'w')
             file.write("Modulation: %s\n"%(self.ui.Modulation.currentIndex()+1))
-            for i in range(self.ui.verticalLayout_6.count()):
-                file.write("%s: %s\n"%(self.ui.verticalLayout_6.itemAt(i).widget().text(),self.ui.verticalLayout_7.itemAt(i).widget().text()))
+            for i in range(self.ui.PuroLabelsLayout.count()):
+                file.write("%s: %s\n"%(self.ui.PuroLabelsLayout.itemAt(i).widget().text(),self.ui.PuroEntriesLayout.itemAt(i).widget().text()))
            
             """ 
             La lista devuelta por children() tiene como 
